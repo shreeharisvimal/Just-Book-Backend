cd8bab4b70a5d1e22c088237923526ec50d479cf
shreeharisvimal <ssvambalapuzha@gmail.com>
2024-07-26 04:27:39 +0530

    bugfix

--- justbook_backend/asgi.py
+++ justbook_backend/asgi.py
@@ -11,10 +11,8 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'justbook_backend.settings')
 django.setup()
 
-django_asgi_app = get_asgi_application()
-
 application = ProtocolTypeRouter({
-    "http": django_asgi_app,
+    "http": get_asgi_application(),
     "websocket": AuthMiddlewareStack(
         URLRouter(
             booking_management.routing.websocket_urlpatterns
